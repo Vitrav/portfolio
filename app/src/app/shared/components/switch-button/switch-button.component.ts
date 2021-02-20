b395c7b9c549bc3fde5a2c6bedc6318a6675ee9d
Dave <dave.visser@outlook.com>
2021-02-20 23:50:01 +0100

    Added images

--- app/src/app/shared/components/switch-button/switch-button.component.ts
+++ app/src/app/shared/components/switch-button/switch-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-switch-button',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./switch-button.component.scss']
 })
 export class SwitchButtonComponent implements OnInit {
-
+  @Input() isChecked = false;
   constructor() { }
 
   ngOnInit(): void {
+   
   }
+
+  checkMode(event: any){
+    console.log(event);
+ }
+//  {
+// //     var mode = <HTMLInputElement>document.getElementById("color-scheme");
+// //     console.log("test");
+// //   if (mode.checked) {
+// //   console.log("DARKMODE")
+// // } else {
+// //  console.log("LIGHTMODE")
+// }
+//   }
+  
 
 }
